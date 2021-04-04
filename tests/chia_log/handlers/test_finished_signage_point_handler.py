f1df38bda27cf49df0ed00da242f918e81eef185
Martin Mihaylov <3739458+martomi@users.noreply.github.com>
2021-04-04 23:56:37 +0200

    Add finished signage point checks
    Check that the full node doesn't skip any signage points. If it does,
    send a normal priority notification.

---
+++ tests/chia_log/handlers/test_finished_signage_point_handler.py
@@ -0,0 +1,51 @@
+# std
+import unittest
+from pathlib import Path
+
+# project
+from src.chia_log.handlers import finished_signage_point_handler
+from src.notifier import EventType, EventService, EventPriority
+
+
+class TestFinishedSignagePointHandler(unittest.TestCase):
+    def setUp(self) -> None:
+        self.handler = finished_signage_point_handler.FinishedSignagePointHandler()
+        self.example_logs_path = Path(__file__).resolve().parents[1] / "logs/finished_signage_point"
+
+    def testNominal(self):
+        with open(self.example_logs_path / "nominal.txt") as f:
+            logs = f.readlines()
+
+        # Currently not generating keep-alive events for the full node
+        # based on the signage points because it's tightly coupled to
+        # the eligible plots check from the harvester
+        for log in logs:
+            events = self.handler.handle(log)
+            self.assertEqual(len(events), 0, "Not expecting any events")
+
+    def testSkippedSignagePoints(self):
+        with open(self.example_logs_path / "skipped.txt") as f:
+            logs = f.readlines()
+
+        expected_list = [(64, 16), (27, 29), (63, 1), (1, 9)]
+        expected_messages = []
+        for expected in expected_list:
+            expected_messages.append(
+                f"Skipped some signage points! Last processed {expected[0]}/64, "
+                f"current {expected[1]}/64. This indicates networking issues."
+            )
+
+        checked = 0
+        for log in logs:
+            events = self.handler.handle(log)
+            if len(events) > 0:
+                self.assertEqual(len(events), 1, "Expected a single event")
+                self.assertEqual(events[0].type, EventType.USER, "Unexpected type")
+                self.assertEqual(events[0].priority, EventPriority.NORMAL, "Unexpected priority")
+                self.assertEqual(events[0].service, EventService.FULL_NODE, "Unexpected service")
+                self.assertEqual(events[0].message, expected_messages[checked], "Unexpected message")
+                checked += 1
+
+
+if __name__ == "__main__":
+    unittest.main()
