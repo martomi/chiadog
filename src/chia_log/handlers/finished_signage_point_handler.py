c890a707eb1be5d83a8e9750b941500c62183d25
Martin Mihaylov <3739458+martomi@users.noreply.github.com>
2021-04-05 13:05:51 +0200

    Signage Points Check: Handle network scramble scenario
    This is scenario observed on actual network. Seems unrelated to local
    node because it's observable from multiple nodes at the same time.
    
    Add some handling to ignore these type of events and reduce the
    resulting false alarms.

--- src/chia_log/handlers/finished_signage_point_handler.py
+++ src/chia_log/handlers/finished_signage_point_handler.py
@@ -56,26 +56,41 @@ class NonSkippedSignagePoints(ConditionChecker):
 
     def __init__(self):
         logging.info("Enabled check for finished signage points.")
-        self._last_processed_signage_point = None
+        self._last_signage_point_timestamp = None
+        self._last_signage_point = None
         self._roll_over_point = 64
 
     def check(self, obj: FinishedSignagePointMessage) -> Optional[Event]:
-        if self._last_processed_signage_point is None:
-            self._last_processed_signage_point = obj.signage_point
+        if self._last_signage_point is None:
+            self._last_signage_point_timestamp = obj.timestamp
+            self._last_signage_point = obj.signage_point
             return None
 
         event = None
-        diff = obj.signage_point - (self._last_processed_signage_point % self._roll_over_point)
+        time_diff_seconds = (obj.timestamp - self._last_signage_point_timestamp).seconds
+        increment_diff = obj.signage_point - (self._last_signage_point % self._roll_over_point)
 
-        if diff <= 0 or diff > 1:
-            message = (
-                f"Experiencing networking issues? Skipped some signage points! "
-                f"Last {self._last_processed_signage_point}/64, current {obj.signage_point}/64."
-            )
-            logging.warning(message)
-            event = Event(
-                type=EventType.USER, priority=EventPriority.NORMAL, service=EventService.FULL_NODE, message=message
-            )
+        if increment_diff <= 0 or increment_diff > 1:
+            # This is hacky addition to prevent false alarms for some network-wide issues that
+            # aren't necessarily related to the local node. See "testNetworkScramble" test case.
+            # Signage points are expected approx every 8-10 seconds. If a point was skipped for real
+            # then we expect the time difference to be at least 2*8 seconds. Otherwise it's flaky event.
+            if time_diff_seconds < 15:
+                logging.info(
+                    f"Detected unusual network activity. Last signage point {self._last_signage_point}, "
+                    f"current signage point {obj.signage_point}. Time difference: {time_diff_seconds} "
+                    f"seconds. Seems unrelated to the local node. Ignoring..."
+                )
+            else:
+                message = (
+                    f"Experiencing networking issues? Skipped some signage points! "
+                    f"Last {self._last_signage_point}/64, current {obj.signage_point}/64."
+                )
+                logging.warning(message)
+                event = Event(
+                    type=EventType.USER, priority=EventPriority.NORMAL, service=EventService.FULL_NODE, message=message
+                )
 
-        self._last_processed_signage_point = obj.signage_point
+        self._last_signage_point_timestamp = obj.timestamp
+        self._last_signage_point = obj.signage_point
         return event
