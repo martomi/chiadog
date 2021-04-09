4550563a5fce0b95b0fc8816b8bb7405c135a757
Art Jacobson <art.jacobson@gmail.com>
2021-04-08 20:08:10 -0500

    Adds script notifier

---
+++ tests/notifier/test_script_notifier.py
@@ -0,0 +1,58 @@
+# std
+import unittest
+
+# project
+from src.notifier import Event, EventType, EventPriority, EventService
+from src.notifier.script_notifier import ScriptNotifier
+
+
+class TestScriptNotifier(unittest.TestCase):
+    def setUp(self) -> None:
+        self.notifier = ScriptNotifier(
+            title_prefix="Test", config={"enable": True, "script_path": "tests/test_script.sh"}
+        )
+
+    def testLowPrioriyNotifications(self):
+        errors = self.notifier.send_events_to_user(
+            events=[
+                Event(
+                    type=EventType.USER,
+                    priority=EventPriority.LOW,
+                    service=EventService.HARVESTER,
+                    message="Low priority notification 1.",
+                ),
+                Event(
+                    type=EventType.USER,
+                    priority=EventPriority.LOW,
+                    service=EventService.HARVESTER,
+                    message="Low priority notification 2.",
+                ),
+            ]
+        )
+        self.assertFalse(errors)
+
+    def testNormalPrioriyNotifications(self):
+        errors = self.notifier.send_events_to_user(
+            events=[
+                Event(
+                    type=EventType.USER,
+                    priority=EventPriority.NORMAL,
+                    service=EventService.HARVESTER,
+                    message="Normal priority notification.",
+                )
+            ]
+        )
+        self.assertFalse(errors)
+
+    def testHighPrioriyNotifications(self):
+        errors = self.notifier.send_events_to_user(
+            events=[
+                Event(
+                    type=EventType.USER,
+                    priority=EventPriority.HIGH,
+                    service=EventService.HARVESTER,
+                    message="This is a high priority notification!",
+                )
+            ]
+        )
+        self.assertFalse(errors)
