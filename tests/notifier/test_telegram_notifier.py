172fa7aa55ea333dac0417d576da7190dfd42edd
Miguel Carranza <miguel@miguelcarranza.es>
2021-04-13 07:30:21 -0700

    Review suggestions

--- tests/notifier/test_telegram_notifier.py
+++ tests/notifier/test_telegram_notifier.py
@@ -17,7 +17,7 @@ class TestTelegramNotifier(unittest.TestCase):
             title_prefix="Test", config={"enable": True, "bot_token": bot_token, "chat_id": chat_id}
         )
 
-    def testTelegramNotifications(self):
+    def testTelegramLowPriorityNotifications(self):
         errors = self.notifier.send_events_to_user(
             events=[
                 Event(
@@ -31,6 +31,44 @@ class TestTelegramNotifier(unittest.TestCase):
                     priority=EventPriority.LOW,
                     service=EventService.HARVESTER,
                     message="Low priority notification 2.",
+                ),
+            ]
+        )
+        self.assertFalse(errors)
+
+    def testTelegramNormalPriorityNotifications(self):
+        errors = self.notifier.send_events_to_user(
+            events=[
+                Event(
+                    type=EventType.USER,
+                    priority=EventPriority.NORMAL,
+                    service=EventService.HARVESTER,
+                    message="Normal priority notification 1.",
+                ),
+                Event(
+                    type=EventType.USER,
+                    priority=EventPriority.NORMAL,
+                    service=EventService.HARVESTER,
+                    message="Normal priority notification 2.",
+                ),
+            ]
+        )
+        self.assertFalse(errors)
+
+    def testTelegramHighPriorityNotifications(self):
+        errors = self.notifier.send_events_to_user(
+            events=[
+                Event(
+                    type=EventType.USER,
+                    priority=EventPriority.HIGH,
+                    service=EventService.HARVESTER,
+                    message="High priority notification 1.",
+                ),
+                Event(
+                    type=EventType.USER,
+                    priority=EventPriority.HIGH,
+                    service=EventService.HARVESTER,
+                    message="High priority notification 2.",
                 ),
             ]
         )
