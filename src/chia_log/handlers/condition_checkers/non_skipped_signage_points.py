a12a4b4c3939a43ead2643fa51c4b94316b04c40
Martin Mihaylov <3739458+martomi@users.noreply.github.com>
2021-04-23 13:46:40 +0200

    Decouple skipped signage points calculations into reusable funciton

--- src/chia_log/handlers/condition_checkers/non_skipped_signage_points.py
+++ src/chia_log/handlers/condition_checkers/non_skipped_signage_points.py
@@ -6,6 +6,7 @@ from typing import Optional
 from . import FinishedSignageConditionChecker
 from src.notifier import Event, EventService, EventType, EventPriority
 from src.chia_log.parsers.finished_signage_point_parser import FinishedSignagePointMessage
+from src.chia_log.handlers.util.calculate_skipped_signage_points import calculate_skipped_signage_points
 
 
 class NonSkippedSignagePoints(FinishedSignageConditionChecker):
@@ -27,29 +28,19 @@ class NonSkippedSignagePoints(FinishedSignageConditionChecker):
             return None
 
         event = None
-        time_diff_seconds = (obj.timestamp - self._last_signage_point_timestamp).seconds
-        increment_diff = obj.signage_point - (self._last_signage_point % self._roll_over_point)
+        skipped = calculate_skipped_signage_points(
+            self._last_signage_point_timestamp, self._last_signage_point, obj.timestamp, obj.signage_point
+        )
 
-        if increment_diff <= 0 or increment_diff > 1:
-            # This is hacky addition to prevent false alarms for some network-wide issues that
-            # aren't necessarily related to the local node. See "testNetworkScramble" test case.
-            # Signage points are expected approx every 8-10 seconds. If a point was skipped for real
-            # then we expect the time difference to be at least 2*8 seconds. Otherwise it's flaky event.
-            if time_diff_seconds < 15:
-                logging.info(
-                    f"Detected unusual network activity. Last signage point {self._last_signage_point}, "
-                    f"current signage point {obj.signage_point}. Time difference: {time_diff_seconds} "
-                    f"seconds. Seems unrelated to the local node. Ignoring..."
-                )
-            else:
-                message = (
-                    f"Experiencing networking issues? Skipped some signage points! "
-                    f"Last {self._last_signage_point}/64, current {obj.signage_point}/64."
-                )
-                logging.warning(message)
-                event = Event(
-                    type=EventType.USER, priority=EventPriority.NORMAL, service=EventService.FULL_NODE, message=message
-                )
+        if skipped > 1:
+            message = (
+                f"Experiencing networking issues? Skipped {skipped} signage points! "
+                f"Last {self._last_signage_point}/64, current {obj.signage_point}/64."
+            )
+            logging.warning(message)
+            event = Event(
+                type=EventType.USER, priority=EventPriority.NORMAL, service=EventService.FULL_NODE, message=message
+            )
 
         self._last_signage_point_timestamp = obj.timestamp
         self._last_signage_point = obj.signage_point
