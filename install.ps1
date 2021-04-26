42837b36d54909829795d346eb18b0a7d6bbee27
skrustev <striker18@mail.bg>
2021-04-27 00:44:24 +0300

    Update windows scripts to support all python variances.

--- install.ps1
+++ install.ps1
@@ -1,11 +1,22 @@
+$command = "py"
+if (Get-Command python -errorAction SilentlyContinue) {
+    $command = "python"
+}
+elseif (Get-Command python3 -errorAction SilentlyContinue) {
+    $command = "python3"
+}
+
 # Create virtual environment
-python -m venv .venv
+Invoke-expression "$($command) -m venv .venv"
+
+# Create virtual environment
+Invoke-expression "$($command) -m venv .venv"
 
 # Activate the virtual environment
 ./.venv/Scripts/activate.ps1
 
 # Update pip to latest version
-python -m pip install --upgrade pip
+Invoke-expression "$($command) -m pip install --upgrade pip"
 
 # Install dependencies
 pip install wheel
--- start.ps1
+++ start.ps1
@@ -1,5 +1,13 @@
+$command = "py"
+if (Get-Command python -errorAction SilentlyContinue) {
+    $command = "python"
+}
+elseif (Get-Command python3 -errorAction SilentlyContinue) {
+    $command = "python3"
+}
+
 # Activate the virtual environment
 ./.venv/Scripts/activate.ps1
 
 # Start the ChiaDog
-python main.py --config config.yaml
+Invoke-expression "$($command) main.py --config config.yaml"
