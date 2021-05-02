ff29f95b3dc0f89a870e7c4a6def3e189809e71e
pieterhelsen <doccie@gmail.com>
2021-05-02 12:53:24 +0200

    Update test_mqtt_notifier.py
    Fixed mypi miss-assignment error for port and qos (int instead of str)

--- tests/notifier/test_mqtt_notifier.py
+++ tests/notifier/test_mqtt_notifier.py
@@ -12,20 +12,20 @@ class TestMqttNotifier(unittest.TestCase):
     def setUp(self) -> None:
 
         host = os.getenv("HOST")
-        port = int(os.getenv("PORT"))
         topic = os.getenv("TOPIC")
         username = os.getenv("MQTT_USERNAME")
         password = os.getenv("MQTT_PASSWORD")
-        qos = int(os.getenv("QOS"))
+        port = int(os.getenv("PORT", 1883))
+        qos = int(os.getenv("QOS", 0))
+        retain = bool(os.getenv("RETAIN", False))
 
         self.assertIsNotNone(host, "You must export HOST as env variable")
         self.assertIsNotNone(port, "You must export PORT as env variable")
         self.assertIsNotNone(topic, "You must export TOPIC as env variable")
 
-        if qos:
-            self.assertIn(
-                qos, [0, 1, 2], "QoS level must be set to 0 (At most once), 1 (at least once) or " "2 (Exactly once)"
-            )
+        self.assertIn(
+            qos, [0, 1, 2], "QoS level must be set to 0 (At most once), 1 (at least once) or " "2 (Exactly once)"
+        )
 
         self.notifier = MqttNotifier(
             title_prefix="Test",
@@ -34,6 +34,7 @@ class TestMqttNotifier(unittest.TestCase):
                 "daily_stats": True,
                 "topic": topic,
                 "qos": qos,
+                "retain": retain,
                 "credentials": {
                     "host": host,
                     "port": port,
