c3b47eff9d2a84d3641b36e1be78fbe6519cacb6
pieterhelsen <doccie@gmail.com>
2021-05-04 23:50:10 +0200

    Install.sh: Add python check
    Added Python check to verify python version is 3.7 or above before proceeding with installation

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
+# Check Python version
+if ! python3 -c 'import sys; assert sys.version_info >= (3,7)' 2> /dev/null;
+then
+  echo 'Found an unsupported version of Python'
+  echo 'Chiadog requires Python 3.7+. Please update before proceeding with the installation'
+  exit 1
+fi
+
 # Create virtual environment
 python3 -m venv venv
 
