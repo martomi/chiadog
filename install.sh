21054403f59a03db66fcf264186f1023622c3a9c
pieterhelsen <doccie@gmail.com>
2021-05-23 13:03:53 +0200

    Add install check for libpython3-dev and build-essential (#171)

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
+# Function that checks if a package is installed locally
+function package_installed() {
+  return dpkg -l "$1" &> /dev/null
+}
+
 # Check Python version
 if ! python3 -c 'import sys; assert sys.version_info >= (3,7)' 2> /dev/null;
 then
   echo 'Found an unsupported version of Python'
   echo 'Chiadog requires Python 3.7+. Please update before proceeding with the installation'
   exit 1
+fi
+
+# Check existence of required python build packages
+if ! package_installed libpython3-dev || ! package_installed build-essential; then
+  sudo apt-get update
+  sudo apt-get install libpython3-dev build-essential -y
 fi
 
 # Create virtual environment
