ea0420ff47e19736a6821ef30d0e80f84651e215
Martin Mihaylov <3739458+martomi@users.noreply.github.com>
2021-05-24 15:52:15 +0200

    NonDecreasingPlots: Increase tolerance threshold to 2
    Closes #147
    Closes #58
    
    I see little value in making refactors and adding additional
    configuration parameters for this. A threshold of 2 should resolve all
    issues that are experienced during copying of plots.
    
    Note that 2 is an instantenous threshold since the max value gets reset
    after that. Meaning that plot count may drop by 10 without a
    notification as long as its dropping in steps of 1.
    
    For transparency of what is happening, I've added an info log that is
    always triggered, regardless of the threshold.
    
    The test case is adjusted to contain a scenario with decreasing plot
    count by 1 which is expected not to trigger a notification.

--- src/chia_log/handlers/condition_checkers/non_decreasing_plots.py
+++ src/chia_log/handlers/condition_checkers/non_decreasing_plots.py
@@ -17,6 +17,9 @@ class NonDecreasingPlots(HarvesterConditionChecker):
     def __init__(self):
         logging.info("Enabled check for non-decreasing total plot count.")
         self._max_farmed_plots = 0
+        # When copying plots it's common that plots decrease by 1 temporarily
+        # by setting the default threshold to 2, we can avoid false alarms
+        self._decrease_warn_threshold = 2
 
     def check(self, obj: HarvesterActivityMessage) -> Optional[Event]:
         if obj.total_plots_count > self._max_farmed_plots:
@@ -25,14 +28,20 @@ class NonDecreasingPlots(HarvesterConditionChecker):
 
         event = None
         if obj.total_plots_count < self._max_farmed_plots:
-            message = (
-                f"Disconnected HDD? The total plot count decreased from "
-                f"{self._max_farmed_plots} to {obj.total_plots_count}."
-            )
-            logging.warning(message)
-            event = Event(
-                type=EventType.USER, priority=EventPriority.HIGH, service=EventService.HARVESTER, message=message
-            )
+            if self._max_farmed_plots - obj.total_plots_count < self._decrease_warn_threshold:
+                logging.info(
+                    f"Plots decreased from {self._max_farmed_plots} to {obj.total_plots_count}. "
+                    f"This is ignored because it's below threshold of {self._decrease_warn_threshold}"
+                )
+            else:
+                message = (
+                    f"Disconnected HDD? The total plot count decreased from "
+                    f"{self._max_farmed_plots} to {obj.total_plots_count}."
+                )
+                logging.warning(message)
+                event = Event(
+                    type=EventType.USER, priority=EventPriority.HIGH, service=EventService.HARVESTER, message=message
+                )
 
         # Update max plots to prevent repeated alarms
         self._max_farmed_plots = obj.total_plots_count
