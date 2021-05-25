ef14eebbf4b701dae6adb68ea2ef91ca97914274
Dave Johnson <davejohnson3000@gmail.com>
2021-05-25 12:42:16 -0700

    Add automated launching for Windows (#91)
    * Add convenient launch script for Windows
    * move platform-specific files to separate dir and automate pathing

---
+++ scripts/win64/start_in_venv.ps1
@@ -0,0 +1,4 @@
+# .Description
+# Launch the watchdog with the configuration and python virtual environment created by the user during installation
+. .\venv\Scripts\activate
+python.exe main.py --config config.yaml
