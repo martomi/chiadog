853af050e32aae54b0df42ec381c38665dd09d94
Dave Johnson <davejohnson3000@gmail.com>
2021-05-25 02:56:30 -0700

    fix script comments

--- start.cmd
+++ start.cmd
@@ -1,2 +1,3 @@
-# Below allows launching chiadog interactively by user while retaining system-wide protection against launching non-interactive scripts
+@echo off
+rem Below allows launching chiadog interactively by user while retaining system-wide protection against launching non-interactive scripts
 powershell -ExecutionPolicy Bypass -File .\scripts\win64\start_in_venv.ps1
