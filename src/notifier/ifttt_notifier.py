229038085db38ff8328e76a7b868e1a2eba076c9
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2021-10-19 14:58:13 -0500

    Ifttt Notifier: cleaning up format

--- src/notifier/ifttt_notifier.py
+++ src/notifier/ifttt_notifier.py
@@ -25,12 +25,7 @@ class IftttNotifier(Notifier):
         for event in events:
             if event.type in self._notification_types and event.service in self._notification_services:
                 conn = http.client.HTTPSConnection("maker.ifttt.com:443", timeout=self._conn_timeout_seconds)
-                request_body = json.dumps(
-                    {
-                    "Message": event.message,
-                    "Title": self.get_title_for_event(event)
-                    }
-                )
+                request_body = json.dumps({"Message": event.message, "Title": self.get_title_for_event(event)})
                 conn.request(
                     "POST",
                     f"/trigger/{self.webhook_name}/json/with/key/{self.token}",
@@ -39,7 +34,7 @@ class IftttNotifier(Notifier):
                         "Content-Type": "application/json",
                         "Accept": "application/json",
                         "API-Key": f"{self.token}",
-                     },
+                    },
                 )
                 response = conn.getresponse()
                 if response.getcode() != 200:
