599dc697ece6fd23cb01da1b1b81d1c20db665af
Guy Davis <guydavis.ca@gmail.com>
2021-11-06 10:46:22 -0600

    Staicoin is 1000x for mojos.

--- src/chia_log/parsers/wallet_added_coin_parser.py
+++ src/chia_log/parsers/wallet_added_coin_parser.py
@@ -44,6 +44,8 @@ class WalletAddedCoinParser:
             mojos = int(match[1])
             if self._prefix == 'chives':
                 mojos = mojos * 10000
+            elif self._prefix == 'staicoin':
+                mojos = mojos * 1000
             parsed_messages.append(
                 WalletAddedCoinMessage(
                     timestamp=dateutil_parser.parse(match[0]),
