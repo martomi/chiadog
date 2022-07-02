3f4947f1edf6752432edd79c86277bd14b48e39a
Martin Mihaylov <3739458+martomi@users.noreply.github.com>
2022-07-02 15:09:02 +0200

    Adjust tests for WalletAddedCoinHandler

--- tests/chia_log/handlers/test_wallet_added_coin_handler.py
+++ tests/chia_log/handlers/test_wallet_added_coin_handler.py
@@ -1,16 +1,26 @@
 # std
 import unittest
 from pathlib import Path
+import copy
 
 # project
 from src.chia_log.handlers.wallet_added_coin_handler import WalletAddedCoinHandler
 from src.notifier import EventType, EventService, EventPriority
+from src.config import Config
 
 
 class TestWalledAddedCoinHandler(unittest.TestCase):
     def setUp(self) -> None:
+        config_dir = Path(__file__).resolve().parents[3]
+        config = Config(config_dir / "config-example.yaml")
+        self.handler_config = config.get_handlers_config()[WalletAddedCoinHandler.config_name()]
+
         self.handler = WalletAddedCoinHandler(config=None)
         self.example_logs_path = Path(__file__).resolve().parents[1] / "logs/wallet_added_coin"
+
+    def testConfig(self):
+        self.assertEqual(self.handler_config["enable"], True)
+        self.assertEqual(self.handler_config["min_transaction_amount"], 0.000000000002)
 
     def testNominal(self):
         with open(self.example_logs_path / "nominal.txt", encoding="UTF-8") as f:
@@ -35,18 +45,18 @@ class TestWalledAddedCoinHandler(unittest.TestCase):
         self.assertEqual(events[0].message, "Cha-ching! Just received 0.000000000001 XCH ☘️")
 
     def testTransactionAmountFilter(self):
-        filter_handler_pass = WalletAddedCoinHandler(
-            config={"enable": "True", "filters": {"transaction_amount": "0.000000000001"}}
-        )
-        filter_handler_fail = WalletAddedCoinHandler(
-            config={"enable": "True", "filters": {"transaction_amount": "0.0000000000001"}}
-        )
+        default_config = self.handler_config
+        no_filter_config = copy.deepcopy(default_config)
+        no_filter_config["min_transaction_amount"] = 0
+
+        filter_handler = WalletAddedCoinHandler(default_config)
+        no_filter_handler = WalletAddedCoinHandler(no_filter_config)
         with open(self.example_logs_path / "small_values.txt", encoding="UTF-8") as f:
             logs = f.readlines()
-        pass_events = filter_handler_pass.handle("".join(logs))
-        self.assertEqual(0, len(pass_events))
-        fail_events = filter_handler_fail.handle("".join(logs))
-        self.assertEqual(1, len(fail_events))
+        filter_events = filter_handler.handle("".join(logs))
+        self.assertEqual(0, len(filter_events))
+        no_filter_events = no_filter_handler.handle("".join(logs))
+        self.assertEqual(1, len(no_filter_events))
 
 
 if __name__ == "__main__":
