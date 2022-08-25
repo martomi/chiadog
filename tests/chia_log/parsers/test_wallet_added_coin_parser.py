e8d7570ec34ab23d88a87f1af0b0043ba5579a4b
Djerfy <djerfy@gmail.com>
2022-08-25 21:22:04 +0200

    Support Chia 1.5.1

--- tests/chia_log/parsers/test_wallet_added_coin_parser.py
+++ tests/chia_log/parsers/test_wallet_added_coin_parser.py
@@ -11,12 +11,14 @@ class TestWalletAddedCoinParser(unittest.TestCase):
         self.parser = WalletAddedCoinParser()
         self.example_logs_path = Path(__file__).resolve().parents[1] / "logs/wallet_added_coin"
         with open(self.example_logs_path / "nominal-before-1.4.0.txt", encoding="UTF-8") as f:
-            self.nominal_logs_before = f.read()
+            self.nominal_logs_before_140 = f.read()
         with open(self.example_logs_path / "nominal-after-1.4.0.txt", encoding="UTF-8") as f:
-            self.nominal_logs_after = f.read()
+            self.nominal_logs_after_140 = f.read()
+        with open(self.example_logs_path / "nominal-after-1.5.1.txt", encoding="UTF-8") as f:
+            self.nominal_logs_after_151 = f.read()
 
     def testBasicParsing(self):
-        for nominal_logs in [self.nominal_logs_before, self.nominal_logs_after]:
+        for nominal_logs in [self.nominal_logs_before_140, self.nominal_logs_after_140, self.nominal_logs_after_151]:
             added_coins = self.parser.parse(nominal_logs)
             total_mojos = 0
             for coin in added_coins:
