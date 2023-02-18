552f3392063ff545f1dd10957f58eb7f988a40d2
WayneWayner <60453944+WayneWayner@users.noreply.github.com>
2023-02-18 01:26:35 +0100

    Add @here for high priority events in Discord (#371)
    * Add @here mention for HIGH priority events
    
    * Formatting
    
    * Add content variable

--- src/notifier/discord_notifier.py
+++ src/notifier/discord_notifier.py
@@ -8,7 +8,7 @@ from typing import List
 from confuse import ConfigView
 
 # project
-from . import Notifier, Event
+from . import Notifier, Event, EventPriority
 
 
 class DiscordNotifier(Notifier):
@@ -25,6 +25,9 @@ class DiscordNotifier(Notifier):
         errors = False
         for event in events:
             if event.type in self._notification_types and event.service in self._notification_services:
+                content = f"**{self.get_title_for_event(event)}**\n{event.message}"
+                if event.priority == EventPriority.HIGH:
+                    content += "\n@here"
                 o = urllib.parse.urlparse(self.webhook_url)
                 conn = http.client.HTTPSConnection(o.netloc, timeout=self._conn_timeout_seconds)
                 conn.request(
@@ -33,7 +36,7 @@ class DiscordNotifier(Notifier):
                     urllib.parse.urlencode(
                         {
                             "username": "chiadog",
-                            "content": f"**{self.get_title_for_event(event)}**\n{event.message}",
+                            "content": content,
                         }
                     ),
                     {"Content-type": "application/x-www-form-urlencoded"},
