3a6316d462cef1f112fc95c3e4c98126047dc26b
Artanicus <artanicus@nocturnal.fi>
2023-02-19 12:21:27 +0200

    Reformat

--- tests/chia_log/handlers/test_wallet_peak_handler.py
+++ tests/chia_log/handlers/test_wallet_peak_handler.py
@@ -14,6 +14,7 @@ from src.notifier import EventType, EventService, EventPriority
 
 logging.basicConfig(level=logging.DEBUG)
 
+
 class TestWalledPeakHandler(unittest.TestCase):
     def setUp(self) -> None:
         config_dir = Path(__file__).resolve().parents[3]
